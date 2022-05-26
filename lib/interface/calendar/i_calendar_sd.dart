4d4ab7712924867ddf8eb0ec45277a8c81cb15cb
Truenya <alenton007@gmail.com>
2022-05-26 19:29:45 +0300

    Исправление отображения календаря.

--- lib/interface/calendar/i_calendar_sd.dart
+++ lib/interface/calendar/i_calendar_sd.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../SimpleSecond/simple_date.dart';
 import 'i_calendar_month_view.dart';
 
 class CalendarPage extends StatelessWidget {
@@ -8,15 +9,17 @@ class CalendarPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final PageController controller = PageController();
     Future<void>.delayed(
-        Duration.zero, () => SDMonth.buildComplete(controller, 10000));
+        Duration.zero,
+        () => SDMonth.buildComplete(
+            controller, 10000 + SimpleDate(DateTime.now()).simpleMonth));
 
     return PageView.builder(
       controller: controller,
       itemBuilder: (context, index) {
         return SDMonth(
-          showedMonth: index,
-          controller: controller,
-        );
+            showedMonth: index,
+            controller: controller,
+            cur: SimpleDate(DateTime.now()));
       },
       // reverse: true,
     );
